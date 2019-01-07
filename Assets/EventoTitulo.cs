29fd32c12a87b689fa82f9f50e3a6a509eef3fd0
Kouharl <victor.soto.mercado@gmail.com>
2019-01-07 00:41:55 -0300

    Titulo listo
    Titulo listo, con boton que cambia de escena al terminar el sonido

---
+++ Assets/EventoTitulo.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EventoTitulo : MonoBehaviour {
+    public AudioSource audio2;
+    public bool Iniciar=false;
+    public bool salir=false;
+
+    void Update()
+    {
+        if (Iniciar==true && !audio2.isPlaying) {
+           SceneManager.LoadScene("Escena_1");
+            Debug.Log("Cambio de escena!");
+        }
+    }
+}
---
+++ Assets/TituloScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TituloScript : MonoBehaviour {
+    public AudioSource Sonido;
+   // public GameObject evento;
+    public bool Iniciar2S = false;
+    public bool Salir2S   = false;
+   // public float condic;
+
+    void Start()
+    { }
+    void Update() {
+
+        if (Iniciar2S==true && !Sonido.isPlaying)
+        {
+            SceneManager.LoadScene("Escena_1");
+            Debug.Log("Cambio de escena!");
+        }
+    }
+
+    public void Iniciar ()
+    {
+        Debug.Log("Iniciando");
+        Sonido.Play();
+        Iniciar2S = true;
+
+
+     
+
+
+    }
+
+public void Salir ()
+    {
+        Debug.Log("Saliendo");
+        Debug.Break();//para poner en pausa. Simular una salida.
+        
+    }
+}
