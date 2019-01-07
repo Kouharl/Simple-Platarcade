8bd224a1f234c7d04f03bc37d0e630197bba80e5
Kouharl <victor.soto.mercado@gmail.com>
2019-01-07 05:34:07 -0300

    Titulo listo y escenario 1 en progreso
    Titulo listo. Escenario 1 en progreso y avanzando con el movimiento del jugador y salto

---
+++ Assets/Script/MovimientoJugador.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoJugador : MonoBehaviour {
+    public float FuerzaDeSalto = 10f;
+    public bool SaltoDisponible = true;
+    public Rigidbody2D RB2D;
+    public int CantidadDeSaltos;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.Space) && SaltoDisponible == true && CantidadDeSaltos < 1) //Saltar
+        {
+            RB2D.AddForce(Vector2.up * FuerzaDeSalto, ForceMode2D.Impulse);
+            CantidadDeSaltos++;
+            //SaltoDisponible = false;
+
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.Space) && SaltoDisponible == true && CantidadDeSaltos < 2) //Segundo salto con menos fuerza
+            {
+                RB2D.AddForce(Vector2.up * (FuerzaDeSalto*0.7f), ForceMode2D.Impulse);
+                CantidadDeSaltos=0;
+                SaltoDisponible = false;
+
+            }
+        }
+
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(new Vector2 (1f*Time.deltaTime,0));
+        }
+        else {
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.Translate(new Vector2(-1f*Time.deltaTime, 0));
+            }
+        }
+       // if (Input.GetKey(KeyCode.W))
+       // {
+       //     transform.Translate(new Vector2(0, 0));
+       // }
+       //else
+       // {
+       //     if (Input.GetKeyDown(KeyCode.S))
+       //     {
+       //         transform.Translate(new Vector2(0, 0));
+       //     }
+       // }
+    }
+}
--- Assets/TituloScript.cs
+++ Assets/TituloScript.cs
@@ -8,7 +8,10 @@ public class TituloScript : MonoBehaviour {
    // public GameObject evento;
     public bool Iniciar2S = false;
     public bool Salir2S   = false;
-   // public float condic;
+    public Animator Transicion;
+    public Animator TransicionBotonInicio;
+    public Animator TransicionBotonSalir;
+    // public float condic;
 
     void Start()
     { }
@@ -23,20 +26,23 @@ public class TituloScript : MonoBehaviour {
 
     public void Iniciar ()
     {
+        if (Iniciar2S==false) { 
         Debug.Log("Iniciando");
         Sonido.Play();
         Iniciar2S = true;
-
-
-     
+        Transicion.SetBool("Iniciando",true); // Inicio condicion para activar la animacion de transicion de escena
+        TransicionBotonInicio.SetBool("Iniciando", true);
+        TransicionBotonSalir.SetBool("Iniciando", true);
+        }
 
 
     }
 
 public void Salir ()
     {
+        if (Salir2S==false && Iniciar2S == false) { 
         Debug.Log("Saliendo");
         Debug.Break();//para poner en pausa. Simular una salida.
-        
     }
+}
 }
